d40693f0fa99cbe6a576a072dae2719910591f45
unknown <oluwadareseyii@gmail.com>
2020-09-16 15:36:10 +0100

    add nav and background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./styles/app.scss";
+
+const pages = [{ path: "/", pathName: "Home", Component: Home }];
 
 function App() {
   return (
     <>
-      <div className="pages"></div>
+      <div className="pages">
+        {pages.map(({ path, pathName, Component }) => (
+          <Route path={path} exact key={pathName}>
+            <Component />
+          </Route>
+        ))}
+      </div>
     </>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { ReactComponent as Cart } from "../assets/images/cart.svg";
+
+const Home = () => {
+  return (
+    <div className="landing">
+      <section className="hero">
+        <nav className="main-nav">
+          <div className="logo">SANTORINIS</div>
+          <div className="nav-items">
+            <div className="nav-item">Home</div>
+            <div className="nav-item">About Us</div>
+            <div className="nav-item">Our Menu</div>
+            <div className="nav-item">Contact Us</div>
+          </div>
+          <div className="cart-button">
+            <Cart />
+          </div>
+        </nav>
+
+        <div className="size-title">SELECT YOUR SIZE</div>
+        <div className="sizing">
+          <div className="size">S</div>
+          <div className="size active">M</div>
+          <div className="size">L</div>
+        </div>
+        <div className="ingredients">
+          <div className="ing ing-1"></div>
+          <div className="ing ing-2"></div>
+          <div className="ing ing-3"></div>
+          <div className="ing ing-4"></div>
+          <div className="ing ing-5"></div>
+          <div className="ing ing-6"></div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
