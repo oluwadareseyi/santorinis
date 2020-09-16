ecbb64be418da1fb9010e7bbb659ccf7254c096e
unknown <oluwadareseyii@gmail.com>
2020-09-16 19:54:21 +0100

    refactor dough container

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Dough from "./Dough";
+import Toppings from "./Toppings";
 
 const Pizza = () => {
   return (
     <div className="pizza-section">
       <Dough />
+      <Toppings />
     </div>
   );
 };
--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Toppings = () => {
     return (
-        <div>
+        <div className="toppings">
             
         </div>
     );
